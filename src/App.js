a0b765993e25051699ad3d36a209213c32eb9b86
github_username <github_email@my.com>
2022-11-23 11:08:30 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState('');
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      axios({
+        method: 'get',
+        url: `https://api.openweathermap.org/data/2.5/weather?zip=${query},fr&appid=65d8db7f5c7724e70536eda38b247eae&units=metric`,
+        data: query
+    })
+    .then(function (reponse) {
+        //On traite la suite une fois la réponse obtenue 
+        setWeather(reponse.data.main.temp)
+        console.log(reponse);
+    })
+    .catch(function (erreur) {
+        //On traite ici les erreurs éventuellement survenues
+        console.log(erreur);
+    });
+      };
+    }
+
+
+
+const dateBuilder = (d)=>{
+  let months = ["December", "January", "February", "March", "April", 
+  "May", "June", "July", "August", "September", "November"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+  "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}  ${date} ${month} ${year}`;
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" 
+            className="search-bar"
+             placeholder="Entrez un code postal"
+             onChange={e => setQuery(e.target.value)}
+             value={query}
+             onKeyPress={search}
+             />
+        </div>  
+        <div className="location-box">
+          <div className="location">
+            {query}
+          </div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            {weather} °C
+          </div>
+        </div>
+      </main>
+             
+
     </div>
   );
 }
